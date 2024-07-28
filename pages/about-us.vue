c2edf787976cc817e8218b1c35d72d7af4a9306b
Oveshen Govender <oveshen.govender@gmail.com>
2024-07-29 06:26:37 +1000

    Added Heroku CORS Proxy

---
+++ pages/about-us.vue
@@ -0,0 +1,19 @@
+<template>
+    <div v-html="content"></div>
+  </template>
+  
+  <script setup>
+  import { ref, onMounted } from 'vue';
+  
+  const content = ref('');
+  
+  onMounted(async () => {
+    try {
+      const { $axios } = useNuxtApp();
+      const response = await $axios.get('/about-us.html');
+      content.value = response.data;
+    } catch (error) {
+      console.error(error);
+    }
+  });
+  </script>  
