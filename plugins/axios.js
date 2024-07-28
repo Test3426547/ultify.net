8efa5c75757677dd20256dde38655dd6dc9e5ff8
Oveshen Govender <oveshen.govender@gmail.com>
2024-07-29 06:10:43 +1000

    Added Heroku CORS Proxy

--- plugins/axios.js
+++ plugins/axios.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export default defineNuxtPlugin((nuxtApp) => {
   const baseURL = process.env.NODE_ENV === 'production'
-    ? 'https://mcdonaldsz.com/'
+    ? 'https://secret-shore-04461-19d2043c008b.herokuapp.com/https://mcdonaldsz.com/'
     : 'http://localhost:3000/';
 
   const axiosInstance = axios.create({
