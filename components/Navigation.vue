f664cea83b51805325947e852c9933ee09123837
Oveshen Govender <oveshen.govender@gmail.com>
2024-07-30 00:23:43 +1000

    Added 2 more html components and updated the navbar

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -1,22 +1,40 @@
 <template>
-  <nav class="navbar navbar-expand-lg">
-    <div class="container-fluid">
-      <NuxtLink class="navbar-brand" to="/" rel="tooltip" title="Designed and Coded by Creative Tim" data-placement="bottom">
-        
+  <nav class="navbar navbar-expand-lg navbar-light bg-white z-index-3 py-3 fixed-top">
+    <div class="container">
+      <NuxtLink class="navbar-brand" to="/" rel="tooltip" title="Ultify Solutions" data-placement="bottom">
+        Ultify Solutions
       </NuxtLink>
-      <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navigation" aria-controls="navigation" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon"></span>
+      <button
+        class="navbar-toggler shadow-none ms-2"
+        type="button"
+        data-bs-toggle="collapse"
+        data-bs-target="#navigation"
+        aria-controls="navigation"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon mt-2">
+          <span class="navbar-toggler-bar bar1"></span>
+          <span class="navbar-toggler-bar bar2"></span>
+          <span class="navbar-toggler-bar bar3"></span>
+        </span>
       </button>
-      <div class="collapse navbar-collapse" id="navigation">
-        <ul class="navbar-nav">
-          <li class="nav-item">
-            <NuxtLink class="nav-link" to="/">Home</NuxtLink>
+      <div class="collapse navbar-collapse w-100 pt-3 pb-2 py-lg-0" id="navigation">
+        <ul class="navbar-nav navbar-nav-hover mx-auto">
+          <li class="nav-item mx-2">
+            <NuxtLink class="nav-link ps-2 d-flex justify-content-between align-items-center" to="/">Home</NuxtLink>
           </li>
-          <li class="nav-item dropdown">
-            <NuxtLink class="nav-link dropdown-toggle" id="dropdownMenuServices" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          <li class="nav-item dropdown mx-2">
+            <a
+              class="nav-link ps-2 d-flex justify-content-between align-items-center dropdown-toggle"
+              id="navbarDropdown"
+              role="button"
+              data-bs-toggle="dropdown"
+              aria-expanded="false"
+            >
               Services
-            </NuxtLink>
-            <ul class="dropdown-menu" aria-labelledby="dropdownMenuServices">
+            </a>
+            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
               <li><NuxtLink class="dropdown-item" to="/services/website">Website</NuxtLink></li>
               <li><NuxtLink class="dropdown-item" to="/services/social-media">Social Media</NuxtLink></li>
               <li><NuxtLink class="dropdown-item" to="/services/seo">SEO</NuxtLink></li>
@@ -25,14 +43,14 @@
               <li><NuxtLink class="dropdown-item" to="/services/print-advertising">Print Advertising</NuxtLink></li>
             </ul>
           </li>
-          <li class="nav-item">
-            <NuxtLink class="nav-link" to="/about-us">About Us</NuxtLink>
+          <li class="nav-item mx-2">
+            <NuxtLink class="nav-link ps-2 d-flex justify-content-between align-items-center" to="/about-us">About Us</NuxtLink>
           </li>
-          <li class="nav-item">
-            <NuxtLink class="nav-link" to="/consultation">Consultation</NuxtLink>
+          <li class="nav-item mx-2">
+            <NuxtLink class="nav-link ps-2 d-flex justify-content-between align-items-center" to="/consultation">Consultation</NuxtLink>
           </li>
-          <li class="nav-item">
-            <NuxtLink class="nav-link" to="/contact-us">Contact Us</NuxtLink>
+          <li class="nav-item mx-2">
+            <NuxtLink class="nav-link ps-2 d-flex justify-content-between align-items-center" to="/contact-us">Contact Us</NuxtLink>
           </li>
         </ul>
       </div>
@@ -46,9 +64,23 @@ import { NuxtLink } from '#components'
 
 <style scoped>
 .navbar {
+  background-color: white;
+}
+
+.navbar-nav-hover .nav-link:hover,
+.navbar-nav-hover .dropdown-toggle:hover {
+  color: #5e72e4;
+}
+
+.fixed-top {
   position: fixed;
   top: 0;
-  width: 100%;
+  left: 0;
+  right: 0;
   z-index: 1030;
+}
+
+.dropdown-menu {
+  z-index: 2000;
 }
 </style>
---
+++ pages/consultation.vue
@@ -0,0 +1,23 @@
+<template>
+    <div v-html="content"></div>
+  </template>
+  
+  <script setup>
+  import { ref, onMounted } from 'vue'
+  import axios from 'axios'
+  
+  const content = ref('')
+  
+  onMounted(async () => {
+    try {
+      const response = await axios.get('/consultation.html')
+      content.value = response.data
+    } catch (error) {
+      console.error('Error fetching HTML content:', error)
+    }
+  })
+  </script>
+  
+  <style scoped>
+  /* Add your styles here */
+  </style>  
