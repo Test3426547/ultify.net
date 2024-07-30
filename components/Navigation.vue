87b5eb75a90f4469dc0c82749c7248338758e968
Oveshen Govender <oveshen.govender@gmail.com>
2024-07-31 04:03:20 +1000

    Updated Navigation.vue

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -1,15 +1,14 @@
 <template>
-    <nav class="fixed w-full top-0 p-2 bg-white bg-opacity-80 backdrop-blur-lg shadow-md z-50 rounded-full md:rounded-lg lg:w-10/12 left-1/2 transform -translate-x-1/2">
+    <nav class="fixed w-full top-2 left-1/2 transform -translate-x-1/2 p-2 bg-white bg-opacity-80 backdrop-blur-lg shadow-md rounded-full z-50 md:rounded-lg md:w-11/12 lg:w-10/12">
       <div class="flex items-center justify-between max-w-screen-lg mx-auto">
         <!-- Header logo -->
-        <div class="flex items-center">
-          <img src="/chatbot-icon.png" alt="Ultify Logo" class="h-10 w-10 mr-2" /> <!-- Replace with your logo path -->
-          <NuxtLink to="/" class="text-lg font-bold text-gray-800">Ultify Solutions</NuxtLink>
-        </div>
+        <NuxtLink to="/" class="text-lg font-bold text-gray-800" aria-label="Home">
+          Ultify Solutions
+        </NuxtLink>
   
         <!-- Mobile toggle -->
         <div class="md:hidden">
-          <button @click="drawer" aria-label="Toggle navigation">
+          <button @click="toggleDrawer" aria-label="Toggle navigation">
             <svg
               class="h-8 w-8 fill-current text-black"
               viewBox="0 0 24 24"
@@ -22,7 +21,7 @@
         <!-- Navbar for larger screens -->
         <div class="hidden md:flex space-x-8 text-sm font-sans items-center">
           <NuxtLink to="/about-us" class="nav-item">About Us</NuxtLink>
-          <div class="relative group" @mouseenter="servicesDropdownOpen = true" @mouseleave="handleMouseLeave">
+          <div class="relative group" @mouseenter="servicesDropdownOpen = true" @mouseleave="servicesDropdownOpen = false">
             <button class="nav-item">
               Services
             </button>
@@ -47,40 +46,39 @@
         enter-active-class="ease-out duration-300"
         leave-active-class="ease-out duration-300"
       >
-        <div v-show="isOpen" class="fixed inset-0 bg-black bg-opacity-50 transition-opacity z-40" @click="drawer">
-          <aside class="fixed inset-y-0 left-0 w-full md:w-64 bg-white p-6 transition-transform duration-300 z-50" :class="{'translate-x-0': isOpen, '-translate-x-full': !isOpen}" @click.stop>
-            <div class="flex justify-between items-center mb-4">
-              <div class="flex items-center">
-                <img src="/chatbot-icon.png" alt="Ultify Logo" class="h-10 w-10 mr-2" /> <!-- Replace with your logo path -->
-                <NuxtLink to="/" class="text-lg font-bold text-gray-800">Ultify Solutions</NuxtLink>
-              </div>
-              <button @click="drawer" aria-label="Close menu">
-                <svg class="w-6 h-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                  <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M6 18L18 6M6 6l12 12" />
+        <div v-show="isOpen" class="z-50 fixed inset-0 bg-black bg-opacity-50 transition-opacity flex items-center justify-center" @click="toggleDrawer">
+          <aside class="w-full max-w-md p-6 bg-white rounded-lg shadow-lg transform transition-transform duration-300" @click.stop :class="{ 'translate-y-0': isOpen, '-translate-y-full': !isOpen }">
+            <div class="flex justify-end">
+              <button @click="toggleDrawer" aria-label="Close menu">
+                <svg
+                  class="w-6 h-6"
+                  viewBox="0 0 24 24"
+                  stroke="currentColor">
+                  <path d="M6 18L18 6M6 6l12 12" />
                 </svg>
               </button>
             </div>
             <nav class="mt-6">
               <ul class="space-y-6">
-                <li><NuxtLink to="/" @click="drawer" class="text-lg font-medium text-gray-900">Home</NuxtLink></li>
-                <li><NuxtLink to="/about-us" @click="drawer" class="text-lg font-medium text-gray-900">About Us</NuxtLink></li>
+                <li><NuxtLink to="/" @click="toggleDrawer" class="text-lg font-medium text-gray-900">Home</NuxtLink></li>
+                <li><NuxtLink to="/about-us" @click="toggleDrawer" class="text-lg font-medium text-gray-900">About Us</NuxtLink></li>
                 <li>
                   <button @click="toggleServicesDropdown" class="text-lg font-medium text-gray-900 w-full text-left">
                     Services
                   </button>
                   <transition name="fade">
                     <ul v-if="servicesDropdownOpen" class="mt-2 pl-4 space-y-2">
-                      <li><NuxtLink to="/website" @click="drawer" class="block">Website</NuxtLink></li>
-                      <li><NuxtLink to="/social-media" @click="drawer" class="block">Social Media</NuxtLink></li>
-                      <li><NuxtLink to="/seo" @click="drawer" class="block">SEO</NuxtLink></li>
-                      <li><NuxtLink to="/paid-media" @click="drawer" class="block">Paid Media</NuxtLink></li>
-                      <li><NuxtLink to="/content-creation" @click="drawer" class="block">Content Creation</NuxtLink></li>
-                      <li><NuxtLink to="/print-advertising" @click="drawer" class="block">Print Advertising</NuxtLink></li>
+                      <li><NuxtLink to="/website" @click="toggleDrawer" class="block">Website</NuxtLink></li>
+                      <li><NuxtLink to="/social-media" @click="toggleDrawer" class="block">Social Media</NuxtLink></li>
+                      <li><NuxtLink to="/seo" @click="toggleDrawer" class="block">SEO</NuxtLink></li>
+                      <li><NuxtLink to="/paid-media" @click="toggleDrawer" class="block">Paid Media</NuxtLink></li>
+                      <li><NuxtLink to="/content-creation" @click="toggleDrawer" class="block">Content Creation</NuxtLink></li>
+                      <li><NuxtLink to="/print-advertising" @click="toggleDrawer" class="block">Print Advertising</NuxtLink></li>
                     </ul>
                   </transition>
                 </li>
-                <li><NuxtLink to="/consultation" @click="drawer" class="text-lg font-medium text-gray-900">Consultation</NuxtLink></li>
-                <li><NuxtLink to="/contact-us" @click="drawer" class="text-lg font-medium text-gray-900">Contact Us</NuxtLink></li>
+                <li><NuxtLink to="/consultation" @click="toggleDrawer" class="text-lg font-medium text-gray-900">Consultation</NuxtLink></li>
+                <li><NuxtLink to="/contact-us" @click="toggleDrawer" class="text-lg font-medium text-gray-900">Contact Us</NuxtLink></li>
               </ul>
             </nav>
           </aside>
@@ -95,25 +93,19 @@
       return {
         isOpen: false,
         servicesDropdownOpen: false,
-        dropdownTimeout: null,
       };
     },
     methods: {
-      drawer() {
+      toggleDrawer() {
         this.isOpen = !this.isOpen;
       },
       toggleServicesDropdown() {
         this.servicesDropdownOpen = !this.servicesDropdownOpen;
       },
-      handleMouseLeave() {
-        this.dropdownTimeout = setTimeout(() => {
-          this.servicesDropdownOpen = false;
-        }, 200); // Small delay before closing
-      },
     },
     watch: {
-      isOpen(val) {
-        if (val) {
+      isOpen(isOpen) {
+        if (isOpen) {
           document.body.style.overflow = 'hidden';
         } else {
           document.body.style.overflow = '';
@@ -165,5 +157,4 @@
     border-radius: 9999px; /* Tailwind rounded-full */
     overflow: hidden; /* Ensure contents are contained */
   }
-  </style>
-  
+  </style>  
