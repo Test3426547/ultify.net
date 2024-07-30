595c15b1ee903a518ebb417b59cc7b583fe633c1
Oveshen Govender <oveshen.govender@gmail.com>
2024-07-30 17:13:04 +1000

    Partially working navbar

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -2,7 +2,7 @@
   <nav class="navbar navbar-expand-lg blur blur-rounded position-absolute my-3 top-0 border-bottom py-3 z-index-3 shadow my-3 py-2 start-0 end-0 mx-4">
     <div class="container">
       <router-link to="/" class="navbar-brand" aria-label="Home">
-        <img src="/path-to-logo.png" alt="Ultify Solutions Logo" class="w-8 h-8 inline-block mr-2"/>
+        <img src="/chatbot-icon.png" alt="Ultify Solutions Logo" class="w-8 h-8 inline-block mr-2"/>
         Ultify Solutions
       </router-link>
       
