d187a545df4a7b78dc7f87298ad0a5e162fda058
Oveshen Govender <oveshen.govender@gmail.com>
2024-07-30 17:28:26 +1000

    Removed icon from navbar

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -1,8 +1,7 @@
 <template>
   <nav class="navbar navbar-expand-lg blur blur-rounded position-absolute my-3 top-0 border-bottom py-3 z-index-3 shadow my-3 py-2 start-0 end-0 mx-4">
     <div class="container">
-      <router-link to="/" class="navbar-brand flex items-center" aria-label="Home">
-        <img src="/chatbot-icon.png" alt="Ultify Solutions Logo" class="hidden lg:inline-block w-10 h-10 mr-2"/>
+      <router-link to="/" class="navbar-brand" aria-label="Home">
         <span class="text-lg font-bold">Ultify Solutions</span>
       </router-link>
       
