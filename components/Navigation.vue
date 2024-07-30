8a3a974500ac779af823a744d70b59b4d7c30ac2
Oveshen Govender <oveshen.govender@gmail.com>
2024-07-30 17:22:46 +1000

    Partially working navbar

--- components/Navigation.vue
+++ components/Navigation.vue
@@ -1,9 +1,9 @@
 <template>
   <nav class="navbar navbar-expand-lg blur blur-rounded position-absolute my-3 top-0 border-bottom py-3 z-index-3 shadow my-3 py-2 start-0 end-0 mx-4">
     <div class="container">
-      <router-link to="/" class="navbar-brand" aria-label="Home">
-        <img src="/chatbot-icon.png" alt="Ultify Solutions Logo" class="inline-block mr-2" style="width: 40px; height: 40px;" />
-        Ultify Solutions
+      <router-link to="/" class="navbar-brand flex items-center" aria-label="Home">
+        <img src="/chatbot-icon.png" alt="Ultify Solutions Logo" class="hidden lg:inline-block w-10 h-10 mr-2"/>
+        <span class="text-lg font-bold">Ultify Solutions</span>
       </router-link>
       
       <button class="navbar-toggler shadow-none ms-2" type="button" @click="toggleMobileMenu">
