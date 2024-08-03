6ac43df7b21c257868d6f3f45756d93066ef7d1f
Oveshen Govender <oveshen.govender@gmail.com>
2024-08-03 13:13:11 +1000

    updated the navbar.vue

--- components/Navbar.vue
+++ components/Navbar.vue
@@ -1,19 +1,30 @@
 <template>
-  <header class="navbar navbar-expand-lg sticky-top py-2 transparent-nav">
+  <header class="navbar navbar-expand-lg sticky-top py-2">
     <div class="container">
       <NuxtLink to="/" class="navbar-brand d-flex align-items-center">
-        <img src="/ultify.svg" width="30" alt="Ultify Solutions">
+        <img src="/ultify.svg" width="30" alt="Ultify Solutions" />
         <span class="ms-2">Ultify Solutions</span>
       </NuxtLink>
-      <button class="navbar-toggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-        <span class="navbar-toggler-icon">
-          <i class="bi bi-list" style="font-size: 1.5rem;"></i>
-        </span>
+      <button
+        class="navbar-toggler"
+        type="button"
+        data-bs-toggle="offcanvas"
+        data-bs-target="#navbarNav"
+        aria-controls="navbarNav"
+        aria-expanded="false"
+        aria-label="Toggle navigation"
+      >
+        <span class="navbar-toggler-icon"></span>
       </button>
       <div class="offcanvas offcanvas-end" tabindex="-1" id="navbarNav">
         <div class="offcanvas-header">
           <h5 class="offcanvas-title">Menu</h5>
-          <button type="button" class="btn-close" data-bs-dismiss="offcanvas" aria-label="Close"></button>
+          <button
+            type="button"
+            class="btn-close"
+            data-bs-dismiss="offcanvas"
+            aria-label="Close"
+          ></button>
         </div>
         <div class="offcanvas-body">
           <ul class="navbar-nav">
@@ -21,7 +32,14 @@
               <NuxtLink to="/about-us" class="nav-link">About Us</NuxtLink>
             </li>
             <li class="nav-item dropdown">
-              <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+              <a
+                class="nav-link dropdown-toggle"
+                href="#"
+                id="navbarDropdown"
+                role="button"
+                data-bs-toggle="dropdown"
+                aria-expanded="false"
+              >
                 Services
               </a>
               <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
@@ -42,8 +60,16 @@
           </ul>
           <div class="mt-3 d-flex align-items-center">
             <label class="form-check-label" for="darkModeSwitch">
-              <input type="checkbox" class="form-check-input" id="darkModeSwitch" @change="toggleDarkMode">
-              <i :class="isDarkMode ? 'bi bi-sun-fill' : 'bi bi-moon-fill'" class="ms-2"></i>
+              <input
+                type="checkbox"
+                class="form-check-input"
+                id="darkModeSwitch"
+                @change="toggleDarkMode"
+              />
+              <i
+                :class="darkMode ? 'fas fa-sun' : 'fas fa-moon'"
+                class="ms-2"
+              ></i>
             </label>
           </div>
         </div>
@@ -56,44 +82,57 @@
 export default {
   data() {
     return {
-      isDarkMode: false,
+      darkMode: false,
     };
   },
   methods: {
     toggleDarkMode() {
-      this.isDarkMode = !this.isDarkMode;
+      this.darkMode = !this.darkMode;
       document.body.classList.toggle('dark-mode');
-    }
-  }
+    },
+  },
 };
 </script>
 
 <style scoped>
 .navbar {
+  background-color: transparent !important;
   padding: 0.5rem 1rem;
-}
-.sticky-top {
-  position: sticky;
+  position: fixed;
   top: 0;
-  z-index: 1020;
+  width: 100%;
+  z-index: 1030;
+  border: none;
 }
-.transparent-nav {
-  background-color: rgba(255, 255, 255, 0.5);
+
+.navbar-toggler {
+  border: none;
 }
-.navbar-toggler-icon i {
-  font-size: 1.5rem;
-}
-.navbar-nav .nav-link {
-  color: #333;
-}
+
 .offcanvas {
-  background-color: rgba(255, 255, 255, 0.9);
+  background-color: transparent;
 }
-.dark-mode .transparent-nav,
-.dark-mode .offcanvas {
-  background-color: rgba(0, 0, 0, 0.5);
+
+.offcanvas-header, .offcanvas-body {
+  background-color: var(--bs-body-bg);
 }
-.dark-mode .navbar-nav .nav-link {
-  color: #fff;
+
+.btn-close {
+  filter: invert(1);
+}
+
+.dark-mode .offcanvas-header,
+.dark-mode .offcanvas-body {
+  background-color: var(--bs-dark);
+}
+
+.dark-mode .navbar-brand,
+.dark-mode .nav-link,
+.dark-mode .offcanvas-title {
+  color: var(--bs-light);
+}
+
+.dark-mode .btn-close {
+  filter: invert(0);
 }
 </style>
