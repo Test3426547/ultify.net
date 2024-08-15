cba8f523984d3251425387ed12baaaf62887acdd
Oveshen Govender <oveshen.govender@gmail.com>
2024-08-15 16:39:17 +1000

    Updated the print-media.vue

---
+++ pages/print-advertising.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+      <HeaderPaidMedia />
+      <PaidMediaTechnology />
+      <PaidMediaDetails />
+      <Consultation />
+      <FAQ />
+      <CTA />
+    </div>
+  </template>
+  
+  <script setup>
+  import HeaderPaidMedia from '@/components/HeaderPaidMedia.vue';
+  import PaidMediaTechnology from '@/components/PaidMediaTechnology.vue';
+  import PaidMediaDetails from '@/components/PaidMediaDetails.vue';
+  import Consultation from '@/components/Consultation.vue';
+  import FAQ from '@/components/FAQ.vue';
+  import CTA from '@/components/CTA.vue';
+  </script>
+  
+  <style>
+  /* Additional styling specific to the Website page */
+  </style>
