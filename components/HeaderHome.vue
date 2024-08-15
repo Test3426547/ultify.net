8cabbbfaf434adbb360eed30f02902c1fbcd488a
Oveshen Govender <oveshen.govender@gmail.com>
2024-08-16 04:24:56 +1000

    Updated the Consultation.vue

--- components/HeaderHome.vue
+++ components/HeaderHome.vue
@@ -10,10 +10,7 @@
           <path id="wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
         </defs>
         <g class="parallax">
-          <use xlink:href="#wave" x="48" y="0" :fill="waveColors[0]" />
-          <use xlink:href="#wave" x="48" y="3" :fill="waveColors[1]" />
-          <use xlink:href="#wave" x="48" y="5" :fill="waveColors[2]" />
-          <use xlink:href="#wave" x="48" y="7" :fill="waveColors[3]" />
+          <use xlink:href="#wave" x="48" y="0" fill="var(--bs-primary)" />
         </g>
       </svg>
     </div>
@@ -21,16 +18,7 @@
 </template>
 
 <script setup>
-import { ref, computed } from 'vue';
-
-const primaryColor = ref(getComputedStyle(document.documentElement).getPropertyValue('--bs-primary').trim());
-
-const waveColors = computed(() => [
-  `${primaryColor.value}B3`, // 70% opacity
-  `${primaryColor.value}80`, // 50% opacity
-  `${primaryColor.value}4D`, // 30% opacity
-  primaryColor.value
-]);
+// No need for computed wave colors anymore
 </script>
 
 <style scoped>
@@ -45,7 +33,7 @@ const waveColors = computed(() => [
 .top-section {
   color: #000000;
   padding: 4rem 2rem;
-  min-height: calc(100vh - 250px); /* Adjusted to account for taller wave container */
+  min-height: calc(100vh - 150px); /* Adjusted for smaller wave container */
   display: flex;
   align-items: center;
   justify-content: center;
@@ -60,28 +48,17 @@ const waveColors = computed(() => [
 /* Wave container styles */
 .wave-container {
   position: relative;
-  height: 250px; /* Increased to accommodate additional padding */
+  height: 150px;
   overflow: hidden;
   background-color: var(--bs-light);
-}
-
-/* Pseudo-element for additional padding below waves */
-.wave-container::after {
-  content: '';
-  position: absolute;
-  bottom: 0;
-  left: 0;
-  right: 0;
-  height: 100px;
-  background-color: var(--bs-primary);
 }
 
 /* Wave SVG styles */
 .waves {
   position: absolute;
-  bottom: 100px; /* Moved up to sit above the new padding */
+  bottom: 0;
   width: 100%;
-  height: 150px;
+  height: 100%;
   min-height: 100px;
   max-height: 150px;
 }
@@ -89,22 +66,6 @@ const waveColors = computed(() => [
 /* Wave animation styles */
 .parallax > use {
   animation: move-forever 25s cubic-bezier(.55,.5,.45,.5) infinite;
-}
-.parallax > use:nth-child(1) {
-  animation-delay: -2s;
-  animation-duration: 7s;
-}
-.parallax > use:nth-child(2) {
-  animation-delay: -3s;
-  animation-duration: 10s;
-}
-.parallax > use:nth-child(3) {
-  animation-delay: -4s;
-  animation-duration: 13s;
-}
-.parallax > use:nth-child(4) {
-  animation-delay: -5s;
-  animation-duration: 20s;
 }
 
 /* Keyframes for wave animation */
@@ -121,6 +82,10 @@ const waveColors = computed(() => [
 @media (max-width: 768px) {
   .top-section {
     padding: 3rem 1rem;
+  }
+  
+  .wave-container {
+    height: 100px;
   }
 }
 </style>
