2747672344ccb241004e6bc3763c2b0d5f962c00
Oveshen Govender <oveshen.govender@gmail.com>
2024-08-15 14:42:40 +1000

    Updated seo.vue

---
+++ pages/seo.vue
@@ -0,0 +1,25 @@
+<template>
+    <div>
+      <HeaderSEO />
+      <SEOTechnology />
+      <SEODetails />
+      <SEOServices />
+      <Consultation />
+      <FAQ />
+      <CTA />
+    </div>
+  </template>
+  
+  <script setup>
+  import HeaderSEO from '@/components/HeaderSEO.vue';
+  import SEOTechnology from '@/components/SEOTechnology.vue';
+  import SEODetails from '@/components/SEODetails.vue';
+  import SEOServices from '@/components/SEOServices.vue';
+  import Consultation from '@/components/Consultation.vue';
+  import FAQ from '@/components/FAQ.vue';
+  import CTA from '@/components/CTA.vue';
+  </script>
+  
+  <style>
+  /* Additional styling specific to the Website page */
+  </style>
