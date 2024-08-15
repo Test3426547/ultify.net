aa0a507f0675d7295323af6faf70357142b09a76
Oveshen Govender <oveshen.govender@gmail.com>
2024-08-15 15:25:28 +1000

    Updated seo.vue

--- pages/social-media.vue
+++ pages/social-media.vue
@@ -1,7 +1,7 @@
 <template>
   <div>
     <HeaderSocialMedia />
-    <WebsiteDetails />
+    <SocialMediaDetails />
     <Consultation />
     <FAQ />
     <CTA />
@@ -10,7 +10,7 @@
 
 <script setup>
 import HeaderSocialMedia from '@/components/HeaderSocialMedia.vue';
-import WebsiteDetails from '@/components/WebsiteDetails.vue';
+import SocialMediaDetails from '@/components/SocialMediaDetails.vue';
 import Consultation from '@/components/Consultation.vue';
 import FAQ from '@/components/FAQ.vue';
 import CTA from '@/components/CTA.vue';
