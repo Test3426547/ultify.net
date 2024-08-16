a30302fbef6e6e2c1ef8a113ddeb172d86b4986e
Oveshen Govender <oveshen.govender@gmail.com>
2024-08-16 14:44:36 +1000

    Update

--- components/HeaderHome.vue
+++ components/HeaderHome.vue
@@ -6,13 +6,18 @@
       </div>
     </div>
     <div class="wave-container">
-      <svg class="waves" xmlns="http://www.w3.org/2000/svg" viewBox="0 24 150 28" preserveAspectRatio="none">
-        <defs>
-          <path id="wave" d="M-160 44c30 0 58-18 88-18s 58 18 88 18 58-18 88-18 58 18 88 18 v44h-352z" />
-        </defs>
-        <g class="parallax">
-          <use xlink:href="#wave" x="48" y="0" :fill="waveColor" />
-        </g>
+      <svg class="waves" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 1440 320" preserveAspectRatio="none">
+        <path :fill="waveColor" fill-opacity="1" d="M0,160L40,165.3C80,171,160,181,240,176C320,171,400,149,480,138.7C560,128,640,128,720,138.7C800,149,880,171,960,181.3C1040,192,1120,192,1200,181.3C1280,171,1360,149,1400,138.7L1440,128L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z">
+          <animate
+            attributeName="d"
+            dur="20s"
+            repeatCount="indefinite"
+            values="
+              M0,160L40,165.3C80,171,160,181,240,176C320,171,400,149,480,138.7C560,128,640,128,720,138.7C800,149,880,171,960,181.3C1040,192,1120,192,1200,181.3C1280,171,1360,149,1400,138.7L1440,128L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z;
+              M0,64L40,85.3C80,107,160,149,240,176C320,203,400,213,480,202.7C560,192,640,160,720,138.7C800,117,880,107,960,128C1040,149,1120,203,1200,213.3C1280,224,1360,192,1400,176L1440,160L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z;
+              M0,160L40,165.3C80,171,160,181,240,176C320,171,400,149,480,138.7C560,128,640,128,720,138.7C800,149,880,171,960,181.3C1040,192,1120,192,1200,181.3C1280,171,1360,149,1400,138.7L1440,128L1440,320L1400,320C1360,320,1280,320,1200,320C1120,320,1040,320,960,320C880,320,800,320,720,320C640,320,560,320,480,320C400,320,320,320,240,320C160,320,80,320,40,320L0,320Z"
+          />
+        </path>
       </svg>
     </div>
   </div>
@@ -27,8 +32,8 @@ let animationFrame;
 const easeInOutQuad = (t) => t < 0.5 ? 2 * t * t : -1 + (4 - 2 * t) * t;
 
 const animateWave = () => {
-  const wave = document.querySelector('.parallax > use');
-  let position = -90;
+  const wave = document.querySelector('.waves path');
+  let time = 0;
   let currentSpeed = 1;
   let targetSpeed = 1;
   let lastSpeedChangeTime = Date.now();
@@ -40,7 +45,7 @@ const animateWave = () => {
 
     // Change speed if it's time
     if (timeSinceLastChange >= speedChangeDuration) {
-      targetSpeed = Math.random() * 2; // New speed between 0 and 2
+      targetSpeed = Math.random() * 1.5 + 0.5; // New speed between 0.5 and 2
       speedChangeDuration = Math.random() * 5000 + 3000; // New duration 3-8 seconds
       lastSpeedChangeTime = now;
     }
@@ -48,20 +53,13 @@ const animateWave = () => {
     // Smoothly adjust current speed towards target speed
     currentSpeed += (targetSpeed - currentSpeed) * 0.05;
 
-    // Move the wave
-    position += 0.5 * currentSpeed;
-    if (position > 85) position = -90;
+    time += 0.005 * currentSpeed;
+    if (time > 1) time -= 1;
 
-    wave.setAttribute('transform', `translate(${position},0)`);
+    const progress = easeInOutQuad(time);
+    wave.setAttribute('transform', `translate(${-1440 * progress},0)`);
 
-    // If speed is very close to 0, pause briefly
-    if (Math.abs(currentSpeed) < 0.05) {
-      setTimeout(() => {
-        animationFrame = requestAnimationFrame(animate);
-      }, 500); // Pause for 0.5 seconds
-    } else {
-      animationFrame = requestAnimationFrame(animate);
-    }
+    animationFrame = requestAnimationFrame(animate);
   };
 
   animate();
