2e27b700b7127ca0d8df8e3da58b3a9df9b1058c
Oveshen Govender <oveshen.govender@gmail.com>
2024-08-17 18:48:34 +1000

    Updated the homepage

--- components/HeaderHome.vue
+++ components/HeaderHome.vue
@@ -3,8 +3,12 @@
     <div class="header__background-top"></div>
     <div class="header__background-bottom"></div>
     <div class="container-fluid h-100">
-      <div class="row h-100">
-        <!-- Content can be added here in the future -->
+      <div class="row h-100 position-relative">
+        <div class="col-12 d-flex align-items-center justify-content-center">
+          <h1 class="header__title text-center fw-bold">
+            Start capitalising on digital<br>resources to elevate your brand.
+          </h1>
+        </div>
       </div>
     </div>
   </header>
@@ -31,5 +35,21 @@
   right: 0;
   height: 65%;
   background-color: var(--bs-primary);
+}
+
+.header__title {
+  position: absolute;
+  bottom: calc(65% + 100px);
+  left: 50%;
+  transform: translateX(-50%);
+  color: var(--bs-primary);
+  font-size: 2.5rem;
+  max-width: 90%;
+}
+
+@media (max-width: 768px) {
+  .header__title {
+    font-size: 1.8rem;
+  }
 }
 </style>
