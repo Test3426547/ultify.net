05cb0613447c9f720dc6b9b9d81bf005aabe578f
Oveshen Govender <oveshen.govender@gmail.com>
2024-08-19 00:12:07 +1000

    Updated the AboutUs.vue

--- components/Directive.vue
+++ components/Directive.vue
@@ -1,16 +1,18 @@
 <template>
-  <section class="py-7 bg-light">
+  <section class="directive-section bg-primary py-7">
     <div class="container">
-      <div class="row">
-        <div class="col-lg-10 mx-auto">
-          <div class="text-center">
-            <h4 class="text-gradient text-primary">Directive message from Ultify -</h4>
-            <blockquote class="blockquote">
-              <p class="mb-0">
-                “Throughout our years in the industry, we have solidified the ins and outs of all digital marketing elements. At ULTIFY, our agency truly understands consumer demands and we always actively remodel our systems to meet such criteria. As a result, we offer our free consultations for the best interest of connecting with like-minded individuals with whom we have built strong connections over the course of their digital journey. We can’t wait to hear from you and excel your innovative ideas through digital marketing with our agency.”
-              </p>
-            </blockquote>
+      <div class="row align-items-center">
+        <div class="col-md-4 mb-4 mb-md-0">
+          <div class="directive-title-wrapper">
+            <h2 class="directive-title bg-light text-dark rounded-pill py-2 px-4 d-inline-block">
+              Directive message from ULTIFY
+            </h2>
           </div>
+        </div>
+        <div class="col-md-8">
+          <p class="directive-message text-white mb-0">
+            "Throughout our years in the industry, we have solidified the ins and outs of all digital marketing elements. At ULTIFY, our agency truly understands consumer demands and we always actively remodel our systems to meet such criteria. As a result, we offer our free consultations for the best interest of connecting with like-minded individuals with whom we have built strong connections over the course of their digital journey. We can't wait to hear from you and excel your innovative ideas through digital marketing with our agency."
+          </p>
         </div>
       </div>
     </div>
@@ -24,31 +26,33 @@ export default {
 </script>
 
 <style scoped>
-.bg-light {
-  background-color: var(--bs-light) !important;
+.directive-section {
+  background-color: var(--bs-primary);
 }
 
-.text-gradient {
-  background: -webkit-linear-gradient(left, var(--bs-blue), var(--bs-indigo));
-  -webkit-background-clip: text;
-  background-clip: initial; /* Replace the comment with a valid CSS property */
-  -webkit-text-fill-color: transparent;
+.directive-title-wrapper {
+  text-align: center;
 }
 
-.blockquote {
-  font-size: 1.25rem;
-  font-weight: 300;
-  line-height: 1.5;
-  margin: 1.5rem 0;
-  border-left: 5px solid var(--bs-primary);
-  padding-left: 1.5rem;
+.directive-title {
+  font-size: 1.5rem;
+  font-weight: bold;
+  background-color: var(--bs-light);
+  color: #000000;
 }
 
-.text-primary {
-  color: var(--bs-primary);
+.directive-message {
+  font-size: 1.1rem;
+  line-height: 1.6;
 }
 
-.mb-0 {
-  margin-bottom: 0;
+@media (max-width: 767px) {
+  .directive-title {
+    font-size: 1.2rem;
+  }
+
+  .directive-message {
+    font-size: 1rem;
+  }
 }
 </style>
