b89abc4ff38f0592c83d8c17e6d62c51df0668f8
Oveshen Govender <oveshen.govender@gmail.com>
2024-08-18 18:44:21 +1000

    Updated the Navbar.vue

--- components/AboutUsDetails.vue
+++ components/AboutUsDetails.vue
@@ -1,58 +1,58 @@
 <template>
-    <div class="about-us-details">
+  <div class="about-us-details">
+    <div class="container">
       <section v-for="(section, index) in sections" :key="index" class="about-section" :class="{ 'text-right': index % 2 !== 0 }">
-        <div class="container">
-          <div class="row align-items-center">
-            <div :class="['col-lg-6', { 'order-lg-2': index % 2 !== 0 }]">
-              <div class="content-wrapper">
-                <div class="number">{{ section.number }}/</div>
-                <div class="pill-wrapper">
-                  <div class="pill">{{ section.title }}</div>
-                </div>
-                <p class="description">{{ section.description }}</p>
+        <div class="row align-items-center">
+          <div :class="['col-lg-6', { 'order-lg-2': index % 2 !== 0 }]">
+            <div class="content-wrapper" :class="{ 'ml-auto': index % 2 !== 0 }">
+              <div class="number">{{ section.number }}/</div>
+              <div class="pill-wrapper">
+                <div class="pill">{{ section.title }}</div>
               </div>
+              <p class="description">{{ section.description }}</p>
             </div>
-            <div :class="['col-lg-6', { 'order-lg-1': index % 2 !== 0 }]">
-              <div class="image-wrapper">
-                <img :src="section.image" :alt="'About Us ' + section.number" class="img-fluid rounded-image">
-              </div>
+          </div>
+          <div :class="['col-lg-6', { 'order-lg-1': index % 2 !== 0 }]">
+            <div class="image-wrapper" :class="{ 'ml-auto': index % 2 === 0 }">
+              <img :src="section.image" :alt="'About Us ' + section.number" class="img-fluid rounded-image">
             </div>
           </div>
         </div>
       </section>
     </div>
-  </template>
-  
-  <script setup>
-  const sections = [
-    {
-      number: '01',
-      title: 'Two minds is better than one',
-      description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
-      image: '/about-us-01.png'
-    },
-    {
-      number: '02',
-      title: 'Not giving up until it\'s perfect',
-      description: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
-      image: '/about-us-02.png'
-    },
-    {
-      number: '03',
-      title: 'Our time is your time',
-      description: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
-      image: '/about-us-03.png'
-    },
-    {
-      number: '04',
-      title: 'Our skills and your idea',
-      description: 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
-      image: '/about-us-04.png'
-    }
-  ];
-  </script>
-  
-  <style scoped>
+  </div>
+</template>
+
+<script setup>
+const sections = [
+  {
+    number: '01',
+    title: 'Two minds is better than one',
+    description: 'Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.',
+    image: '/about-us-01.png'
+  },
+  {
+    number: '02',
+    title: 'Not giving up until it\'s perfect',
+    description: 'Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.',
+    image: '/about-us-02.png'
+  },
+  {
+    number: '03',
+    title: 'Our time is your time',
+    description: 'Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur.',
+    image: '/about-us-03.png'
+  },
+  {
+    number: '04',
+    title: 'Our skills and your idea',
+    description: 'Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.',
+    image: '/about-us-04.png'
+  }
+];
+</script>
+
+<style scoped>
 .about-us-details {
   background-color: var(--bs-light);
   padding: 80px 0;
@@ -76,10 +76,10 @@
 }
 
 .row {
-  margin-left: -15px;
-  margin-right: -15px;
   display: flex;
   align-items: center;
+  margin-left: -15px;
+  margin-right: -15px;
 }
 
 .col-lg-6 {
@@ -90,6 +90,10 @@
 
 .content-wrapper {
   max-width: 90%;
+}
+
+.content-wrapper.ml-auto {
+  margin-left: auto;
 }
 
 .number {
@@ -119,8 +123,12 @@
 }
 
 .image-wrapper {
-  padding: 0;
-  width: 540px;
+  width: 100%;
+  max-width: 540px;
+}
+
+.image-wrapper.ml-auto {
+  margin-left: auto;
 }
 
 .rounded-image {
@@ -129,28 +137,6 @@
   width: 100%;
   height: 400px;
   object-fit: cover;
-}
-
-/* Adjust image positioning */
-.about-section:nth-child(4n+1) .image-wrapper,
-.about-section:nth-child(4n+3) .image-wrapper {
-  margin-left: 50px;
-}
-
-.about-section:nth-child(4n+2) .image-wrapper,
-.about-section:nth-child(4n) .image-wrapper {
-  margin-right: 50px;
-  margin-left: auto;
-}
-
-/* Ensure text alignment */
-.about-section:nth-child(odd) .content-wrapper {
-  padding-right: 50px;
-}
-
-.about-section:nth-child(even) .content-wrapper {
-  margin-left: auto;
-  padding-left: 50px;
 }
 
 @media (max-width: 1399.98px) {
@@ -176,13 +162,9 @@
   .content-wrapper {
     max-width: 100%;
     margin-bottom: 2rem;
-    padding-left: 0 !important;
-    padding-right: 0 !important;
   }
 
   .image-wrapper {
-    width: 100%;
-    max-width: 540px;
     margin-left: auto !important;
     margin-right: auto !important;
   }
