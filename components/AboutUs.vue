0e0daf16190f1e0aaca41c91120c2c505483f940
Oveshen Govender <oveshen.govender@gmail.com>
2024-08-18 21:00:35 +1000

    Updated the Navbar.vue

--- components/AboutUs.vue
+++ components/AboutUs.vue
@@ -14,14 +14,14 @@
           <div class="dna-helix">
             <div class="strand strand-1"></div>
             <div class="strand strand-2"></div>
-            <div class="rung rung-1">Innovation</div>
-            <div class="rung rung-2">Creativity</div>
-            <div class="rung rung-3">Excellence</div>
-            <div class="rung rung-4">Integrity</div>
-            <div class="rung rung-5">Passion</div>
-            <div class="rung rung-6">Collaboration</div>
-            <div class="rung rung-7">Adaptability</div>
-            <div class="rung rung-8">Results-Driven</div>
+            <div class="rung rung-1"><span>Innovation</span></div>
+            <div class="rung rung-2"><span>Creativity</span></div>
+            <div class="rung rung-3"><span>Excellence</span></div>
+            <div class="rung rung-4"><span>Integrity</span></div>
+            <div class="rung rung-5"><span>Passion</span></div>
+            <div class="rung rung-6"><span>Collaboration</span></div>
+            <div class="rung rung-7"><span>Adaptability</span></div>
+            <div class="rung rung-8"><span>Results-Driven</span></div>
           </div>
         </div>
       </div>
@@ -52,49 +52,54 @@ export default {
 .dna-helix {
   position: relative;
   width: 100%;
-  height: 400px;
+  height: 500px;
   overflow: hidden;
 }
 
 .strand {
   position: absolute;
-  width: 10px;
-  height: 150%;
+  width: 20px;
+  height: 200%;
   background-color: var(--bs-white);
 }
 
 .strand-1 {
-  left: 45%;
-  transform: translateX(-50%) rotate(30deg);
+  left: 50%;
+  transform: translateX(-50%) rotate(20deg);
 }
 
 .strand-2 {
-  right: 45%;
-  transform: translateX(50%) rotate(-30deg);
+  left: 50%;
+  transform: translateX(-50%) rotate(-20deg);
 }
 
 .rung {
   position: absolute;
   left: 50%;
-  transform: translateX(-50%);
   width: 120px;
-  height: 20px;
-  background-color: var(--bs-light);
-  color: var(--bs-primary);
+  height: 30px;
   display: flex;
   align-items: center;
   justify-content: center;
-  font-weight: bold;
 }
 
-.rung-1 { top: 5%; }
-.rung-2 { top: 15%; }
-.rung-3 { top: 30%; }
-.rung-4 { top: 40%; }
-.rung-5 { top: 50%; }
-.rung-6 { top: 60%; }
-.rung-7 { top: 75%; }
-.rung-8 { top: 85%; }
+.rung span {
+  background-color: var(--bs-light);
+  color: var(--bs-primary);
+  padding: 5px 10px;
+  border-radius: 15px;
+  font-weight: bold;
+  font-size: 0.9em;
+}
+
+.rung-1 { top: 5%; transform: translateX(-50%) rotate(-20deg); }
+.rung-2 { top: 15%; transform: translateX(-50%) rotate(20deg); }
+.rung-3 { top: 30%; transform: translateX(-50%) rotate(-20deg); }
+.rung-4 { top: 40%; transform: translateX(-50%) rotate(20deg); }
+.rung-5 { top: 50%; transform: translateX(-50%) rotate(-20deg); }
+.rung-6 { top: 60%; transform: translateX(-50%) rotate(20deg); }
+.rung-7 { top: 75%; transform: translateX(-50%) rotate(-20deg); }
+.rung-8 { top: 85%; transform: translateX(-50%) rotate(20deg); }
 
 @media (max-width: 768px) {
   .container {
@@ -103,7 +108,15 @@ export default {
   }
   
   .dna-helix {
-    height: 300px;
+    height: 400px;
+  }
+
+  .rung {
+    width: 100px;
+  }
+
+  .rung span {
+    font-size: 0.8em;
   }
 }
 </style>
