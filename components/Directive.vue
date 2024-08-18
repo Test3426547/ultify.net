6f1b8a5d8217295e2f6e95ddd238c711e59bceb8
Oveshen Govender <oveshen.govender@gmail.com>
2024-08-19 01:36:16 +1000

    Updated the AboutUs.vue

--- components/Directive.vue
+++ components/Directive.vue
@@ -1,19 +1,21 @@
 <template>
-  <section class="directive-section bg-primary py-7">
-    <div class="container">
-      <div class="row align-items-center">
-        <div class="col-md-4 mb-4 mb-md-0">
-          <div class="directive-title-wrapper">
-            <h2 class="directive-title bg-light text-dark rounded-pill py-2 px-4 d-inline-block">
-              Directive message from ULTIFY
-            </h2>
+  <section class="directive-section">
+    <div class="container position-relative">
+      <div class="directive-box bg-white rounded-lg shadow-lg p-4 p-md-5">
+        <div class="row align-items-center">
+          <div class="col-md-4 mb-4 mb-md-0">
+            <div class="directive-title-wrapper">
+              <h2 class="directive-title bg-primary text-white rounded-pill py-2 px-4 d-inline-block">
+                Directive message from ULTIFY
+              </h2>
+            </div>
           </div>
-        </div>
-        <div class="col-md-8">
-          <div class="directive-message-wrapper">
-            <p class="directive-message text-white mb-0">
-              "Throughout our years in the industry, we have solidified the ins and outs of all digital marketing elements. At ULTIFY, our agency truly understands consumer demands and we always actively remodel our systems to meet such criteria. As a result, we offer our free consultations for the best interest of connecting with like-minded individuals with whom we have built strong connections over the course of their digital journey. We can't wait to hear from you and excel your innovative ideas through digital marketing with our agency."
-            </p>
+          <div class="col-md-8">
+            <div class="directive-message-wrapper">
+              <p class="directive-message text-dark mb-0">
+                "Throughout our years in the industry, we have solidified the ins and outs of all digital marketing elements. At ULTIFY, our agency truly understands consumer demands and we always actively remodel our systems to meet such criteria. As a result, we offer our free consultations for the best interest of connecting with like-minded individuals with whom we have built strong connections over the course of their digital journey. We can't wait to hear from you and excel your innovative ideas through digital marketing with our agency."
+              </p>
+            </div>
           </div>
         </div>
       </div>
@@ -29,42 +31,40 @@ export default {
 
 <style scoped>
 .directive-section {
-  background-color: var(--bs-primary);
+  margin: -50px 0; /* Adjust this value to control how much it overlaps */
+  position: relative;
+  z-index: 10;
+}
+
+.directive-box {
+  border-radius: 20px;
 }
 
 .directive-title-wrapper {
-  text-align: center;
+  text-align: left;
 }
 
 .directive-title {
-  font-size: 1.5rem;
+  font-size: 1.2rem;
   font-weight: bold;
-  background-color: var(--bs-light);
-  color: #000000;
-}
-
-.directive-message-wrapper {
-  padding-left: 150px;
 }
 
 .directive-message {
-  font-size: 1.1rem;
+  font-size: 1rem;
   line-height: 1.6;
 }
 
-@media (max-width: 991px) {
-  .directive-message-wrapper {
-    padding-left: 0;
-  }
-}
-
 @media (max-width: 767px) {
+  .directive-section {
+    margin: -30px 0; /* Less overlap on mobile */
+  }
+  
   .directive-title {
-    font-size: 1.2rem;
+    font-size: 1rem;
   }
 
   .directive-message {
-    font-size: 1rem;
+    font-size: 0.9rem;
   }
 }
 </style>
