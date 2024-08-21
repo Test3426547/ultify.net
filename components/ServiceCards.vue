c7384b6e48ee032d065621a997399bf175c7623a
Oveshen Govender <oveshen.govender@gmail.com>
2024-08-21 14:54:11 +1000

    Updated the consultation.vue

--- components/ServiceCards.vue
+++ components/ServiceCards.vue
@@ -1,16 +1,14 @@
 <template>
   <div class="service-cards-section position-relative bg-light">
     <div class="container h-100 d-flex align-items-center justify-content-center">
-      <div class="row w-100">
-        <div class="col-12 col-md-6 mb-4 mb-md-0">
-          <!-- Added custom class for black background and extreme rounding -->
-          <!-- Moved 50px to the left using negative margin -->
-          <div class="card custom-black custom-rounded shadow-lg h-100 ms-n5"></div>
+      <div class="row w-100 justify-content-between"> <!-- Added justify-content-between -->
+        <div class="col-12 col-md-5 mb-4 mb-md-0"> <!-- Changed col-md-6 to col-md-5 -->
+          <!-- Removed ms-n5 class -->
+          <div class="card custom-black custom-rounded shadow-lg h-100"></div>
         </div>
-        <div class="col-12 col-md-6">
-          <!-- Added custom class for black background and extreme rounding -->
-          <!-- Moved 50px to the right using positive margin -->
-          <div class="card custom-black custom-rounded shadow-lg h-100 me-n5"></div>
+        <div class="col-12 col-md-5"> <!-- Changed col-md-6 to col-md-5 -->
+          <!-- Removed me-n5 class -->
+          <div class="card custom-black custom-rounded shadow-lg h-100"></div>
         </div>
       </div>
     </div>
@@ -38,27 +36,17 @@
 
 .card {
   aspect-ratio: 16 / 9;
-  /* Removed hover animation */
 }
 
-/* Custom class for black background */
 .custom-black {
   background-color: #000000;
 }
 
-/* Custom class for extreme rounded corners */
 .custom-rounded {
   border-radius: 30px;
 }
 
-/* Custom margin classes for spacing */
-.ms-n5 {
-  margin-left: -50px;
-}
-
-.me-n5 {
-  margin-right: -50px;
-}
+/* Removed custom margin classes ms-n5 and me-n5 */
 
 @media (max-width: 991.98px) {
   .service-cards-section {
@@ -75,8 +63,7 @@
     aspect-ratio: 1 / 1;
   }
 
-  /* Reset margins on mobile to prevent overflow */
-  .ms-n5, .me-n5 {
+  .row {
     margin-left: 0;
     margin-right: 0;
   }
