9941b0dc031568eb304a10d10ceacaf012e64454
Oveshen Govender <oveshen.govender@gmail.com>
2024-08-21 14:59:50 +1000

    Updated the consultation.vue

--- components/ServiceCards.vue
+++ components/ServiceCards.vue
@@ -1,14 +1,12 @@
 <template>
   <div class="service-cards-section position-relative bg-light">
-    <div class="container h-100 d-flex align-items-center justify-content-center">
-      <div class="row w-100 justify-content-between"> <!-- Added justify-content-between -->
-        <div class="col-12 col-md-5 mb-4 mb-md-0"> <!-- Changed col-md-6 to col-md-5 -->
-          <!-- Removed ms-n5 class -->
-          <div class="card custom-black custom-rounded shadow-lg h-100"></div>
+    <div class="container-fluid h-100 d-flex align-items-center justify-content-center"> <!-- Changed to container-fluid for more width -->
+      <div class="row w-100 justify-content-between">
+        <div class="col-12 col-lg-5 mb-4 mb-lg-0"> <!-- Changed breakpoint to lg -->
+          <div class="card custom-black custom-rounded shadow-lg custom-size"></div> <!-- Added custom-size class -->
         </div>
-        <div class="col-12 col-md-5"> <!-- Changed col-md-6 to col-md-5 -->
-          <!-- Removed me-n5 class -->
-          <div class="card custom-black custom-rounded shadow-lg h-100"></div>
+        <div class="col-12 col-lg-5"> <!-- Changed breakpoint to lg -->
+          <div class="card custom-black custom-rounded shadow-lg custom-size"></div> <!-- Added custom-size class -->
         </div>
       </div>
     </div>
@@ -28,39 +26,45 @@
   overflow: hidden;
 }
 
-.container {
-  max-width: 1200px;
+.container-fluid {
+  max-width: 1400px; /* Increased max-width to accommodate larger cards */
   margin: 0 auto;
   padding: 0 15px;
 }
 
-.card {
-  aspect-ratio: 16 / 9;
-}
-
 .custom-black {
-  background-color: #2b2a2a;
+  background-color: #2B2A2A;
 }
 
 .custom-rounded {
   border-radius: 30px;
 }
 
-/* Removed custom margin classes ms-n5 and me-n5 */
+/* New class for custom size */
+.custom-size {
+  width: calc(100% + 100px); /* 100px wider */
+  height: 0;
+  padding-bottom: calc(56.25% + 200px); /* 16:9 aspect ratio + 200px taller */
+  margin-left: -50px; /* Offset half of the extra width */
+  margin-right: -50px; /* Offset half of the extra width */
+}
 
 @media (max-width: 991.98px) {
   .service-cards-section {
     min-height: 100vh;
+  }
+  
+  .custom-size {
+    width: 100%; /* Full width on smaller screens */
+    padding-bottom: 100%; /* Square aspect ratio on smaller screens */
+    margin-left: 0;
+    margin-right: 0;
   }
 }
 
 @media (max-width: 768px) {
   .service-cards-section {
     min-height: 100vh;
-  }
-  
-  .card {
-    aspect-ratio: 1 / 1;
   }
 
   .row {
