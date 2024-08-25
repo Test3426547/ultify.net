6946f4104f748e1c1238990813cad9d4276648d4
Oveshen Govender <oveshen.govender@gmail.com>
2024-08-25 13:34:02 +1000

    Updated the HeaderHome.vue

--- components/HeaderHome.vue
+++ components/HeaderHome.vue
@@ -43,24 +43,26 @@
             <div class="consultation-form-inner p-4">
               <h2 class="text-center text-dark mb-4">Book A Free Consultation Now</h2>
               <div class="spacer"></div>
-              <form @submit.prevent="handleSubmit">
-                <div class="form-group mb-3">
-                  <input class="form-control" v-model="form.businessName" placeholder="URL/Business Name (if applicable)" type="text" />
-                </div>
-                <div class="form-group mb-3">
-                  <input class="form-control" v-model="form.name" placeholder="Name" type="text" />
-                </div>
-                <div class="form-group mb-3">
-                  <input class="form-control" v-model="form.email" placeholder="Email" type="email" />
-                </div>
-                <div class="form-group mb-3">
-                  <input class="form-control" v-model="form.phone" placeholder="Phone" type="tel" />
-                </div>
-                <button type="submit" class="btn btn-primary btn-block">
-                  LEAD WITHOUT A SWEAT
-                </button>
-              </form>
-              <p class="disclaimer text-dark mt-3 text-center small">
+              <div class="form-container">
+                <form @submit.prevent="handleSubmit">
+                  <div class="form-group mb-3">
+                    <input class="form-control" v-model="form.businessName" placeholder="URL/Business Name (if applicable)" type="text" />
+                  </div>
+                  <div class="form-group mb-3">
+                    <input class="form-control" v-model="form.name" placeholder="Name" type="text" />
+                  </div>
+                  <div class="form-group mb-3">
+                    <input class="form-control" v-model="form.email" placeholder="Email" type="email" />
+                  </div>
+                  <div class="form-group mb-3">
+                    <input class="form-control" v-model="form.phone" placeholder="Phone" type="tel" />
+                  </div>
+                  <button type="submit" class="btn btn-primary btn-block">
+                    LEAD WITHOUT A SWEAT
+                  </button>
+                </form>
+              </div>
+              <p class="disclaimer text-dark mt-3 text-center">
                 You are booking a free consultation with no maximum time (TnC's apply). We will call you on the given number on our first available time-slot.
               </p>
             </div>
@@ -203,6 +205,10 @@ const handleSubmit = () => {
   overflow-y: auto;
 }
 
+.form-container {
+  padding: 0 25px;
+}
+
 .spacer {
   height: 50px;
 }
@@ -237,6 +243,7 @@ form {
 
 .disclaimer {
   margin-top: 1rem;
+  font-size: 0.7rem;
 }
 
 .header__scroll-arrow {
